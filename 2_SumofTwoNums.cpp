3903ac4715994ebbb516417cdae980690a017325
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-21 23:35:33 +0530

    Update and rename

---
+++ 2_SumofTwoNums.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, sum;    //Declaring variables for two numbers and sum
+    cout << "Enter first number: ";    //To prompt the user to input number
+    cin >> n1;    //Gets the input value and stores in variable n1
+    cout << "Enter second number: ";
+    cin >> n2;
+
+    sum = n1 + n2;    //Calculating sum
+
+    cout << "The sum is: " << sum;    //Printing the sum
+
+}
