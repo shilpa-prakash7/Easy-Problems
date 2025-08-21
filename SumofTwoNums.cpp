f9c522cfd13cb31e338c8160d4fa9ddfa673ea0d
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-21 23:30:52 +0530

    Add files via upload

---
+++ SumofTwoNums.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2, sum;
+    cout << "Enter first number: ";
+    cin >> n1;
+    cout << "Enter second number: ";
+    cin >> n2;
+
+    sum = n1 + n2;
+
+    cout << "The sum is: " << sum;
+}
