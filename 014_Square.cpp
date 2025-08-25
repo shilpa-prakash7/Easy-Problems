f82467e2a6b4f5cb25cb9e7dddd2348d4ef90daa
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-25 21:38:30 +0530

    Add files via upload

---
+++ 014_Square.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int num;    //Declaring variable num
+
+    cout << "Enter a number to be squared: ";
+    cin >> num;   //Gets the number to be squared from user
+
+    cout << "\nSquare of " << num << " = " << num*num;  //Prints the square using num*num
+}
