d0611356b0ad7d7b9e010a11775b1eda15ccecf5
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-28 16:11:50 +0530

    Add files via upload

---
+++ 021_NaturalNumbers.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "The first 10 Natural Numbers - \n\n";
+
+    for (int i=1; i<=10; i++)   //Using For loop to print the first 10 natural numbers
+    {
+        cout << i << endl;  //Prints each number one by one
+    }
+}
