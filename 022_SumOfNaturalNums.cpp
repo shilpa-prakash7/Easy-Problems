5b081df6b044415d10fa7ffd0469c5f4cf37e6b2
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-28 16:26:38 +0530

    Update

--- 022_SumOfNaturalNums.cpp
+++ 022_SumOfNaturalNums.cpp
@@ -7,7 +7,7 @@ int main()
     int n, sum = 0; //Declaring variable for n and initiating sum as 0
 
     cout << "Enter how many natural numbers to be added: ";
-    cin >> n;    //Stores the user input in num variable
+    cin >> n;    //Stores the user input in variable n
 
     for (int i=1; i<=n; i++)  //Using For loop to go through the first n natural numbers
     {
@@ -16,4 +16,5 @@ int main()
 
     //Printing the sum of n numbers
     cout << "\nThe sum of first " << n << " Natural numbers = " << sum;
+
 }
