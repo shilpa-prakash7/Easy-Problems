296fe304d9aebaaa2440e33a851c96f1281ac868
shilpa-prakash7 <shilpaprakashk7@gmail.com>
2025-08-28 16:28:57 +0530

    Add files via upload

---
+++ 023_ProdOfNaturalNums.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, prod = 1; //Declaring variable for n and initiating prod as 1
+
+    cout << "Enter how many natural numbers to be multiplied: ";
+    cin >> n;    //Stores the user input in variable n
+
+    for (int i=1; i<=n; i++)  //Using For loop to go through the first n natural numbers
+    {
+        prod *= i;  //Multiplies each number and stores in prod
+    }
+
+    //Printing the product of n numbers
+    cout << "\nThe product of first " << n << " Natural numbers = " << prod;
+}
